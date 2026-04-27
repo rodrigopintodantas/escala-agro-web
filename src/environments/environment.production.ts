49dd74d1d6ecea27254ead5d3c8f11a32d30d9ee
Rodrigo Pinto Dantas <rodrigo.pdantas@tjdft.jus.br>
2026-04-27 10:14:14 -0300

    fix: produção

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,13 +1,8 @@
 ﻿export const environment = {
     production: true,
-    apiUrl: 'https://escala-agro.tjdft.jus.br/api',
+    /** URL pública da API em produção (sem barra no final). */
+    /*apiUrl: 'https://sgea.digital/api'*/
+    apiUrl: '/api',
     ambiente: 'production',
-    useSimpleAuth: true,
-    keyCloakConfig: {
-        redirect_uri: 'https://escala-agro.tjdft.jus.br',
-        urlsso: 'https://sso.tjdft.jus.br/auth/',
-        client: 'sgpj-web',
-        realm: 'SUDES',
-        urlPattern: /^https:\/\/escala-agro.tjdft.jus.br\/.*$/
-    }
-};
+    useSimpleAuth: true
+  };
